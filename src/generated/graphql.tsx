b4225422c0428abf949f88bd15c97e90708a2874
Jeffrey Rose <jtr219@outlook.com>
2021-11-04 11:30:26 -0400

    update server port

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -14,6 +14,212 @@ export type Scalars = {
   Float: number;
 };
 
+export type Consoles = {
+  __typename?: 'CONSOLES';
+  console: Scalars['String'];
+  full_name: Scalars['String'];
+  year_released: Scalars['Int'];
+  global_sales?: Maybe<Scalars['Float']>;
+  maker: Scalars['String'];
+  console_type: Scalars['String'];
+};
+
+export type Console_Makers = {
+  __typename?: 'CONSOLE_MAKERS';
+  maker: Scalars['String'];
+};
+
+/** The columns that can be used to group the search by */
+export enum Column {
+  Genre = 'GENRE',
+  Rating = 'RATING',
+  Console = 'CONSOLE',
+  Title = 'TITLE',
+  Publisher = 'PUBLISHER',
+  YearOfRelease = 'YEAR_OF_RELEASE'
+}
+
+export type ConsoleGameSales = {
+  __typename?: 'ConsoleGameSales';
+  global_sales: Scalars['Float'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  grouping: Array<Scalars['String']>;
+  console: Scalars['String'];
+  year_of_release?: Maybe<Scalars['Int']>;
+  genre?: Maybe<Scalars['String']>;
+  rating?: Maybe<Scalars['String']>;
+  publisher?: Maybe<Scalars['String']>;
+};
+
+export type CrossPlatformSales = {
+  __typename?: 'CrossPlatformSales';
+  global_sales: Scalars['Float'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  grouping: Array<Scalars['String']>;
+  console?: Maybe<Scalars['String']>;
+  year_of_release?: Maybe<Scalars['Int']>;
+  genre?: Maybe<Scalars['String']>;
+  rating?: Maybe<Scalars['String']>;
+  publisher?: Maybe<Scalars['String']>;
+  title: Scalars['String'];
+};
+
+export type GamesWithGroupBy = {
+  __typename?: 'GamesWithGroupBy';
+  id: Scalars['Int'];
+  title: Scalars['String'];
+  console: Console;
+  year_of_release: Scalars['Int'];
+  genre: Genre;
+  publisher: Scalars['String'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  global_sales: Scalars['Float'];
+  critic_score?: Maybe<Scalars['Int']>;
+  critic_count?: Maybe<Scalars['Int']>;
+  user_score?: Maybe<Scalars['Int']>;
+  user_count?: Maybe<Scalars['Int']>;
+  developer?: Maybe<Scalars['String']>;
+  rating?: Maybe<Rating>;
+  grouping: Array<Scalars['String']>;
+};
+
+export type GenreSales = {
+  __typename?: 'GenreSales';
+  global_sales: Scalars['Float'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  grouping: Array<Scalars['String']>;
+  console?: Maybe<Scalars['String']>;
+  year_of_release?: Maybe<Scalars['Int']>;
+  genre: Scalars['String'];
+  rating?: Maybe<Scalars['String']>;
+  publisher?: Maybe<Scalars['String']>;
+};
+
+export type OrderByColumn = {
+  __typename?: 'OrderByColumn';
+  column: OrderByColumns;
+  order: SortOrder;
+};
+
+export type OrderByColumnInput = {
+  column: OrderByColumns;
+  order: SortOrder;
+};
+
+/** select columns to order results by */
+export enum OrderByColumns {
+  Title = 'TITLE',
+  Console = 'CONSOLE',
+  YearOfRelease = 'YEAR_OF_RELEASE',
+  Publisher = 'PUBLISHER',
+  Genre = 'GENRE',
+  Rating = 'RATING',
+  CriticScore = 'CRITIC_SCORE',
+  UserScore = 'USER_SCORE',
+  Developer = 'DEVELOPER',
+  GlobalSales = 'GLOBAL_SALES',
+  NaSales = 'NA_SALES',
+  EuSales = 'EU_SALES',
+  JpSales = 'JP_SALES',
+  OtherSales = 'OTHER_SALES'
+}
+
+export type PaginatedConsoleGameSales = {
+  __typename?: 'PaginatedConsoleGameSales';
+  rows: Array<ConsoleGameSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedCrossPlatformSales = {
+  __typename?: 'PaginatedCrossPlatformSales';
+  rows: Array<CrossPlatformSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedGames = {
+  __typename?: 'PaginatedGames';
+  rows: Array<GamesWithGroupBy>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedGenreSales = {
+  __typename?: 'PaginatedGenreSales';
+  rows: Array<GenreSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedPublisherSales = {
+  __typename?: 'PaginatedPublisherSales';
+  rows: Array<PublisherSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedQueryOptions = {
+  where: WhereOptions;
+  groupBy?: Maybe<Array<Column>>;
+  /**
+   * if query uses group by columns,
+   *       then order by columns must be present
+   *       amongst group by options
+   */
+  orderBy?: Maybe<Array<OrderByColumnInput>>;
+  limit: Scalars['Int'];
+  offset: Scalars['Int'];
+};
+
+export type PaginatedRatingSales = {
+  __typename?: 'PaginatedRatingSales';
+  rows: Array<RatingSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PaginatedYearSales = {
+  __typename?: 'PaginatedYearSales';
+  rows: Array<YearSales>;
+  groupedBy: Array<Column>;
+  orderedBy: Array<OrderByColumn>;
+  hasMore: Scalars['Boolean'];
+};
+
+export type PublisherSales = {
+  __typename?: 'PublisherSales';
+  global_sales: Scalars['Float'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  grouping: Array<Scalars['String']>;
+  console?: Maybe<Scalars['String']>;
+  year_of_release?: Maybe<Scalars['Int']>;
+  genre?: Maybe<Scalars['String']>;
+  rating?: Maybe<Scalars['String']>;
+  publisher: Scalars['String'];
+};
+
 export type Query = {
   __typename?: 'Query';
   games: PaginatedGames;
@@ -80,39 +286,67 @@ export type QueryHighestUserScoresArgs = {
   options: PaginatedQueryOptions;
 };
 
-export type PaginatedGames = {
-  __typename?: 'PaginatedGames';
-  rows: Array<GamesWithGroupBy>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type GamesWithGroupBy = {
-  __typename?: 'GamesWithGroupBy';
-  id: Scalars['Int'];
-  title: Scalars['String'];
-  console: Console;
-  year_of_release: Scalars['Int'];
-  genre: Genre;
-  publisher: Scalars['String'];
+export type RatingSales = {
+  __typename?: 'RatingSales';
+  global_sales: Scalars['Float'];
   na_sales: Scalars['Float'];
   eu_sales: Scalars['Float'];
   jp_sales: Scalars['Float'];
   other_sales: Scalars['Float'];
-  global_sales: Scalars['Float'];
-  critic_score?: Maybe<Scalars['Int']>;
-  critic_count?: Maybe<Scalars['Int']>;
-  user_score?: Maybe<Scalars['Int']>;
-  user_count?: Maybe<Scalars['Int']>;
-  developer?: Maybe<Scalars['String']>;
-  rating?: Maybe<Rating>;
   grouping: Array<Scalars['String']>;
+  console?: Maybe<Scalars['String']>;
+  year_of_release?: Maybe<Scalars['Int']>;
+  genre?: Maybe<Scalars['String']>;
+  rating?: Maybe<Scalars['String']>;
+  publisher?: Maybe<Scalars['String']>;
+};
+
+/** select whether column should be sorted as ascending or descending */
+export enum SortOrder {
+  Asc = 'ASC',
+  Desc = 'DESC'
+}
+
+export type TextSearch = {
+  searchText: Array<Scalars['String']>;
+  searchType: TextSearchType;
+};
+
+export type WhereOptions = {
+  title?: Maybe<TextSearch>;
+  console?: Maybe<Array<Console>>;
+  year_of_release?: Maybe<Array<Scalars['Int']>>;
+  publisher?: Maybe<Array<Scalars['String']>>;
+  genre?: Maybe<Array<Genre>>;
+  rating?: Maybe<Array<Rating>>;
+  critic_score?: Maybe<Array<Scalars['Int']>>;
+  user_score?: Maybe<Array<Scalars['Int']>>;
+  developer?: Maybe<Array<Scalars['String']>>;
+  global_sales?: Maybe<Array<Scalars['Float']>>;
+  na_sales?: Maybe<Array<Scalars['Float']>>;
+  eu_sales?: Maybe<Array<Scalars['Float']>>;
+  jp_sales?: Maybe<Array<Scalars['Float']>>;
+  other_sales?: Maybe<Array<Scalars['Float']>>;
+};
+
+export type YearSales = {
+  __typename?: 'YearSales';
+  global_sales: Scalars['Float'];
+  na_sales: Scalars['Float'];
+  eu_sales: Scalars['Float'];
+  jp_sales: Scalars['Float'];
+  other_sales: Scalars['Float'];
+  grouping: Array<Scalars['String']>;
+  console?: Maybe<Scalars['String']>;
+  year_of_release: Scalars['Int'];
+  genre?: Maybe<Scalars['String']>;
+  rating?: Maybe<Scalars['String']>;
+  publisher?: Maybe<Scalars['String']>;
 };
 
 /** select games by different consoles */
 export enum Console {
-  '2600' = '_2600',
+  '_2600' = '_2600',
   '3Do' = '_3DO',
   '3Ds' = '_3DS',
   Dc = 'DC',
@@ -173,81 +407,6 @@ export enum Rating {
   T = 'T'
 }
 
-/** The columns that can be used to group the search by */
-export enum Column {
-  Genre = 'GENRE',
-  Rating = 'RATING',
-  Console = 'CONSOLE',
-  Title = 'TITLE',
-  Publisher = 'PUBLISHER',
-  YearOfRelease = 'YEAR_OF_RELEASE'
-}
-
-export type OrderByColumn = {
-  __typename?: 'OrderByColumn';
-  column: OrderByColumns;
-  order: SortOrder;
-};
-
-/** select columns to order results by */
-export enum OrderByColumns {
-  Title = 'TITLE',
-  Console = 'CONSOLE',
-  YearOfRelease = 'YEAR_OF_RELEASE',
-  Publisher = 'PUBLISHER',
-  Genre = 'GENRE',
-  Rating = 'RATING',
-  CriticScore = 'CRITIC_SCORE',
-  UserScore = 'USER_SCORE',
-  Developer = 'DEVELOPER',
-  GlobalSales = 'GLOBAL_SALES',
-  NaSales = 'NA_SALES',
-  EuSales = 'EU_SALES',
-  JpSales = 'JP_SALES',
-  OtherSales = 'OTHER_SALES'
-}
-
-/** select whether column should be sorted as ascending or descending */
-export enum SortOrder {
-  Asc = 'ASC',
-  Desc = 'DESC'
-}
-
-export type PaginatedQueryOptions = {
-  where: WhereOptions;
-  groupBy?: Maybe<Array<Column>>;
-  /**
-   * if query uses group by columns,
-   *       then order by columns must be present
-   *       amongst group by options
-   */
-  orderBy?: Maybe<Array<OrderByColumnInput>>;
-  limit: Scalars['Int'];
-  offset: Scalars['Int'];
-};
-
-export type WhereOptions = {
-  title?: Maybe<TextSearch>;
-  console?: Maybe<Array<Console>>;
-  year_of_release?: Maybe<Array<Scalars['Int']>>;
-  publisher?: Maybe<Array<Scalars['String']>>;
-  genre?: Maybe<Array<Genre>>;
-  rating?: Maybe<Array<Rating>>;
-  critic_score?: Maybe<Array<Scalars['Int']>>;
-  user_score?: Maybe<Array<Scalars['Int']>>;
-  developer?: Maybe<Array<Scalars['String']>>;
-  global_sales?: Maybe<Array<Scalars['Float']>>;
-  na_sales?: Maybe<Array<Scalars['Float']>>;
-  eu_sales?: Maybe<Array<Scalars['Float']>>;
-  jp_sales?: Maybe<Array<Scalars['Float']>>;
-  other_sales?: Maybe<Array<Scalars['Float']>>;
-};
-
-export type TextSearch = {
-  searchText: Array<Scalars['String']>;
-  searchType: TextSearchType;
-};
-
 /** distinguish between searching for exact text, text appearing at start, at end, or included anywhere */
 export enum TextSearchType {
   Equals = 'EQUALS',
@@ -255,165 +414,6 @@ export enum TextSearchType {
   Startswith = 'STARTSWITH',
   Endswith = 'ENDSWITH'
 }
-
-export type OrderByColumnInput = {
-  column: OrderByColumns;
-  order: SortOrder;
-};
-
-export type PaginatedCrossPlatformSales = {
-  __typename?: 'PaginatedCrossPlatformSales';
-  rows: Array<CrossPlatformSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type CrossPlatformSales = {
-  __typename?: 'CrossPlatformSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console?: Maybe<Scalars['String']>;
-  year_of_release?: Maybe<Scalars['Int']>;
-  genre?: Maybe<Scalars['String']>;
-  rating?: Maybe<Scalars['String']>;
-  publisher?: Maybe<Scalars['String']>;
-  title: Scalars['String'];
-};
-
-export type PaginatedGenreSales = {
-  __typename?: 'PaginatedGenreSales';
-  rows: Array<GenreSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type GenreSales = {
-  __typename?: 'GenreSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console?: Maybe<Scalars['String']>;
-  year_of_release?: Maybe<Scalars['Int']>;
-  genre: Scalars['String'];
-  rating?: Maybe<Scalars['String']>;
-  publisher?: Maybe<Scalars['String']>;
-};
-
-export type PaginatedYearSales = {
-  __typename?: 'PaginatedYearSales';
-  rows: Array<YearSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type YearSales = {
-  __typename?: 'YearSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console?: Maybe<Scalars['String']>;
-  year_of_release: Scalars['Int'];
-  genre?: Maybe<Scalars['String']>;
-  rating?: Maybe<Scalars['String']>;
-  publisher?: Maybe<Scalars['String']>;
-};
-
-export type PaginatedPublisherSales = {
-  __typename?: 'PaginatedPublisherSales';
-  rows: Array<PublisherSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type PublisherSales = {
-  __typename?: 'PublisherSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console?: Maybe<Scalars['String']>;
-  year_of_release?: Maybe<Scalars['Int']>;
-  genre?: Maybe<Scalars['String']>;
-  rating?: Maybe<Scalars['String']>;
-  publisher: Scalars['String'];
-};
-
-export type PaginatedConsoleGameSales = {
-  __typename?: 'PaginatedConsoleGameSales';
-  rows: Array<ConsoleGameSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type ConsoleGameSales = {
-  __typename?: 'ConsoleGameSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console: Scalars['String'];
-  year_of_release?: Maybe<Scalars['Int']>;
-  genre?: Maybe<Scalars['String']>;
-  rating?: Maybe<Scalars['String']>;
-  publisher?: Maybe<Scalars['String']>;
-};
-
-export type PaginatedRatingSales = {
-  __typename?: 'PaginatedRatingSales';
-  rows: Array<RatingSales>;
-  groupedBy: Array<Column>;
-  orderedBy: Array<OrderByColumn>;
-  hasMore: Scalars['Boolean'];
-};
-
-export type RatingSales = {
-  __typename?: 'RatingSales';
-  global_sales: Scalars['Float'];
-  na_sales: Scalars['Float'];
-  eu_sales: Scalars['Float'];
-  jp_sales: Scalars['Float'];
-  other_sales: Scalars['Float'];
-  grouping: Array<Scalars['String']>;
-  console?: Maybe<Scalars['String']>;
-  year_of_release?: Maybe<Scalars['Int']>;
-  genre?: Maybe<Scalars['String']>;
-  rating?: Maybe<Scalars['String']>;
-  publisher?: Maybe<Scalars['String']>;
-};
-
-export type Consoles = {
-  __typename?: 'CONSOLES';
-  console: Scalars['String'];
-  full_name: Scalars['String'];
-  year_released: Scalars['Int'];
-  global_sales?: Maybe<Scalars['Float']>;
-  maker: Scalars['String'];
-  console_type: Scalars['String'];
-};
-
-export type Console_Makers = {
-  __typename?: 'CONSOLE_MAKERS';
-  maker: Scalars['String'];
-};
 
 export type ConsoleSalesQueryVariables = Exact<{
   options: PaginatedQueryOptions;
--- src/utils/urqlClient.ts
+++ src/utils/urqlClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'urql'
 
 export const client = createClient({
-  url: 'http://localhost:5000/graphql',
+  url: 'http://localhost:8000/graphql',
 })
