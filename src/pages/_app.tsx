79862248f47dc8429d61b88d9faa23f7ff812204
Jeffrey Rose <jtr219@outlook.com>
2021-11-06 13:38:52 -0400

    replace framer-motion wrapper with chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,18 @@
 import { Provider } from 'urql'
 import { client } from '../utils/urqlClient'
-import { AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
 import '../styles/globals.css'
-
-function handleExitComplete() {
-  if (typeof window !== 'undefined') {
-    window.scrollTo({ top: 0 })
-  }
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
   return (
     <Provider value={client}>
-      <AnimatePresence exitBeforeEnter onExitComplete={handleExitComplete}>
+      <ChakraProvider resetCSS >
         <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-      <style>
-        {`
-        body {
-          padding: 0;
-          margin: 0;
-          background: #f9fbf8;
-        }
-
-        * {
-          box-sizing: border-box;
-          font-family: Helvetica, sans-serif;
-          font-weight: 900;
-          color: #222;
-        }
-      `}
-      </style>
+      </ChakraProvider>
     </Provider>
   )
 }
