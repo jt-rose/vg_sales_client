89bae544a14c5d56e359b7dd24c21dd58b19f092
Jeffrey Rose <jtr219@outlook.com>
2021-11-09 13:01:10 -0500

    restructure form to use tabs

--- src/components/form/QueryForm.tsx
+++ src/components/form/QueryForm.tsx
@@ -1,35 +1,33 @@
-import { Stack, Heading /*useRangeSlider*/ } from "@chakra-ui/react";
-import { TextSearchType, Rating } from "../../generated/graphql";
-//import { RegisterOptions, useForm } from "react-hook-form";
+import {
+  Heading,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { GameForm } from "./GameForm";
 import { ScoreForm } from "./ScoreForm";
 import { SalesForm } from "./SalesForm";
 import { useState } from "react";
-
-export type FormData = {
-  title?: string;
-  textSearchType: TextSearchType;
-  consoles?: Console[];
-  yearsRange: [number, number];
-  publishers: string[];
-  developers: string[];
-  genres: string[];
-  ratings: Rating[];
-  criticScoreRange: [number, number];
-  userScoreRange: [number, number];
-
-  // sales
-  globalSalesRange: [number, number];
-  NASalesRange: [number, number];
-  JPNSalesRange: [number, number];
-  EUSalesRange: [number, number];
-  OtherSalesRange: [number, number];
-};
+import { FormData } from "./FormData";
 
 // a top level form component that houses
 // subforms to adjust the search fields
 // according to game, scores, and sales data
 export const QueryForm = () => {
-  //const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit } = useForm<FormData>();
+
+  // set up state for game parameters
+  /*const [ title, updateTitle] = useState<string>('')
+  const [ , updateTitle] = useState<string>('')
+  const [ title, updateTitle] = useState<string>('')
+  const [ title, updateTitle] = useState<string>('')
+  const [ startingYear, updateStartingYear] = useState<number>(1970)
+  const [ endingYear, updateEndingYear] = useState<number>(2016)
+  const [ publishers, updatePublishers] = useState<string[]>([])
+  const [ developers, updateDevelopers] = useState<string[]>([])*/
 
   // set up state for critic and user scores
   const [criticScoresRange, updateCriticScoresRange] = useState<number[]>([
@@ -55,30 +53,45 @@ export const QueryForm = () => {
   const { setValueAtIndex } = actions;
 */
   return (
-    <form>
-      <Stack>
-        <Heading>Query Form</Heading>
-        <div>Game</div>
-        <ScoreForm
-          criticScoresRange={criticScoresRange}
-          updateCriticScoresRange={updateCriticScoresRange}
-          userScoresRange={userScoresRange}
-          updateUserScoresRange={updateUserScoresRange}
-        />
-        <SalesForm
-          globalSales={globalSales}
-          updateGlobalSales={updateGlobalSales}
-          NASales={NASales}
-          updateNASales={updateNASales}
-          JPNSales={JPNSales}
-          updateJPNSales={updateJPNSales}
-          EUSales={EUSales}
-          updateEUSales={updateEUSales}
-          otherRegionSales={otherRegionSales}
-          updateOtherRegionSales={updateOtherRegionSales}
-        />
-      </Stack>
-      <button>submit</button>
-    </form>
+    <Tabs>
+      <Heading>Query Form</Heading>
+      <TabList>
+        <Tab>Game</Tab>
+        <Tab>Scores</Tab>
+        <Tab>Sales</Tab>
+      </TabList>
+      <form onSubmit={handleSubmit(() => {})}>
+        <TabPanels>
+          <TabPanel>
+            <GameForm register={register} />
+          </TabPanel>
+
+          <TabPanel>
+            <ScoreForm
+              criticScoresRange={criticScoresRange}
+              updateCriticScoresRange={updateCriticScoresRange}
+              userScoresRange={userScoresRange}
+              updateUserScoresRange={updateUserScoresRange}
+            />
+          </TabPanel>
+
+          <TabPanel>
+            <SalesForm
+              globalSales={globalSales}
+              updateGlobalSales={updateGlobalSales}
+              NASales={NASales}
+              updateNASales={updateNASales}
+              JPNSales={JPNSales}
+              updateJPNSales={updateJPNSales}
+              EUSales={EUSales}
+              updateEUSales={updateEUSales}
+              otherRegionSales={otherRegionSales}
+              updateOtherRegionSales={updateOtherRegionSales}
+            />
+          </TabPanel>
+        </TabPanels>
+        <button>submit</button>
+      </form>
+    </Tabs>
   );
 };
