6a2c924425cbaae894862239d31bf40963d196f7
Jeffrey Rose <jtr219@outlook.com>
2021-11-16 22:51:47 -0500

    connect redux store to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "../utils/apolloClient";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "../redux/store";
 import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -10,9 +12,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
     <ApolloProvider client={client}>
-      <ChakraProvider resetCSS>
-        <Component {...pageProps} key={router.route} />
-      </ChakraProvider>
+      <ReduxProvider store={store}>
+        <ChakraProvider resetCSS>
+          <Component {...pageProps} key={router.route} />
+        </ChakraProvider>
+      </ReduxProvider>
     </ApolloProvider>
   );
 }
