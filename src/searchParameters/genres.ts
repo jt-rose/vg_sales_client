8365e5df571dcb6e04975e6e73aefe8c5defb841
Jeffrey Rose <jtr219@outlook.com>
2021-11-16 21:43:05 -0500

    add complete list of available genres

---
+++ src/searchParameters/genres.ts
@@ -0,0 +1,16 @@
+import { Genre } from "../generated/graphql";
+
+export const allGenres: Genre[] = [
+  Genre.Action,
+  Genre.Adventure,
+  Genre.Fighting,
+  Genre.Misc,
+  Genre.Platform,
+  Genre.Puzzle,
+  Genre.Racing,
+  Genre.Roleplaying,
+  Genre.Shooter,
+  Genre.Simulation,
+  Genre.Sports,
+  Genre.Strategy,
+];
