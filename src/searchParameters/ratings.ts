bc71c2e6c0ef29e5578a590c288e266277496917
Jeffrey Rose <jtr219@outlook.com>
2021-11-16 21:43:29 -0500

    add complete list of available ratings

---
+++ src/searchParameters/ratings.ts
@@ -0,0 +1,12 @@
+import { Rating } from "../generated/graphql";
+
+export const allRatings: Rating[] = [
+  Rating.Ao,
+  Rating.E,
+  Rating.E10Plus,
+  Rating.Ec,
+  Rating.KA,
+  Rating.M,
+  Rating.Rp,
+  Rating.T,
+];
