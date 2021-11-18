0fd864444c42837e7f62af76ed69ae9a0532e5d8
Jeffrey Rose <jtr219@outlook.com>
2021-11-18 11:33:45 -0500

    remove unused tailwind global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { useRouter } from "next/router";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import "../styles/globals.css";
-
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
